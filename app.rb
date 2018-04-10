2b242a160fbe6264e7cb8a46f4f37d7896aee847
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-04-10 19:09:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/input' do
+  get '/' do
     erb :user_input
   end
 
-  post '/piglatin' do
+  post '/piglatinize' do
     @inputs = params
     erb :user_input
   end
