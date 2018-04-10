2afaaf876b39c0ce3e76fd19a34584901b8406cb
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-04-10 20:00:50 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,8 +1,57 @@
 class PigLatinizer
-  attr_reader :word
-
-  def piglatinize(word)
-    if word.start_with?('a','e','i','o','u')
-      
+  def self.translate(phrase)
+    words = phrase.split(" ")
+    words.collect {|word| interpret(word)}.join(" ")
   end
+
+  private
+
+  def self.interpret(word)
+    special_vowel_translate(word) ||
+    basic_vowel_sound_translate(word) ||
+    special_three_letter_translate(word) ||
+    special_two_letter_translate(word) ||
+    simple_translate(word)
+  end
+
+  def self.rotation(num, word)
+    word.split("").rotate(num).join + "ay"
+  end
+
+  def self.special_vowel_translate(word)
+    if word[0..1] == "ye"
+      rotation(1, word)
+    end
+  end
+
+  def self.basic_vowel_sound_translate(word)
+    if word[0].vowel? || (word[0] == 'x' && !word[1].vowel?)
+      rotation(0, word)
+    end
+  end
+
+  def self.special_three_letter_translate(word)
+    if %w(squ thr sch).include? word[0..2]
+      rotation(3, word)
+    end
+  end
+
+  def self.special_two_letter_translate(word)
+    if %w(ch qu th).include? word[0..1]
+      rotation(2, word)
+    end
+  end
+
+  def self.simple_translate(word)
+    rotation(1, word)
+  end
+
+end
+
+class String
+
+  def vowel?
+    %w(a e i o u y A E I O U Y).include? self
+  end
+
 end
