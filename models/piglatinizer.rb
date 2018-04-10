8a77435d3e54d12cd01a14a9a0cdd361eb915e85
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-04-10 19:25:02 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,10 +1,14 @@
 class PigLatinizer
   attr_reader :word
 
-  def piglatin(word)
-    if word =~ (/\A[aeiou]/i)
-      word = word + 'ay'
-    elsif word =~ (/\A[^aeiou]/i)
+  def split_sentence(word)
+    words = word.split(" ")
+  end 
+  
+  def piglatin(words)
+    if words =~ (/\A[aeiou]/i)
+      words = word + 'ay'
+    elsif words =~ (/\A[^aeiou]/i)
       match = /\A[^aeiou]/i.match(word)
       word = match.post_match + match.to_s + 'ay'
     end
