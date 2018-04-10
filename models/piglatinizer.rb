4825603674a1cff53f8cc8cb3ac97385ae8606df
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-04-10 20:11:58 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,15 +1,15 @@
 class PigLatinizer
-  
+
   def piglatinize(word)
     vowels = %w{ a e i o u}
-    
-    words_array=word.split  
+
+    words_array=word.split
     return_string =''
-    
+
     final=''
-    
+
     words_array.each do |word|
-      i=0  
+      i=0
       ending=''
       array=word.split(//)
       if(vowels.include?(array[i]))
@@ -17,11 +17,11 @@ class PigLatinizer
         return_string<<word
         if(words_array.size>1)
           return_string << ' '
-        end            
+        end
       else
         while(!vowels.include?(array[i]))
           letter= array.delete_at(i)
-          ending << letter              
+          ending << letter
         end
         array.push(ending)
         word=array.push('ay').join('')
@@ -33,4 +33,4 @@ class PigLatinizer
     end
     return return_string.rstrip
   end
-  
+end
